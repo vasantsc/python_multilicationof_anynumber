c40ab701617e3dc6086e9b4d6fa509d0a38931ce
vasantsc <91326379+vasantsc@users.noreply.github.com>
2022-06-17 11:47:56 +0530

    Add files via upload

---
+++ multipication.py
@@ -0,0 +1,8 @@
+n=int(input("enter the number for its multiplication\n"))
+i=1
+print("*****************************")
+for i in range(11):
+    if i==0:
+        continue
+    print(n,"x",i,"=",i*n)
+print("*****************************")
